14cf8a32ed7520b2052e14412a8bb5cce4ad7e54
garrickf <gfaerr@stanford.edu>
2017-09-14 11:35:41 -0700

    add form voting

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,10 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -19,8 +20,24 @@ def detail(request, question_id):
 	return render(request, 'polls/detail.html', {'question': question}) # context is inside
 
 def results(request, question_id):
-	response = "You're looking at the results of question %s"
-	return HttpResponse(response % question_id)
+    # response = "You're looking at the results of question %s"
+	# return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s" % question_id)
+	# return HttpResponse("You're voting on question %s" % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# redisp. voting form
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice."
+			})
+	else:
+		selected_choice.votes += 1 # careful: race condition (if two vote at same time)
+		selected_choice.save()
+		# return HttpResponseRedirect
+		return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
