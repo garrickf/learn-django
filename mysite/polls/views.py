b442bf2808876ba942096a55f7517bc46147eea2
garrickf <gfaerr@stanford.edu>
2017-09-14 12:05:17 -0700

    refactor code to support generic list and detail views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,24 +6,25 @@ from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {'latest_question_list': latest_question_list}
-	return render(request, 'polls/index.html', context) # with this shortcut, we no longer need to import HttpResponse or loader!
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-	# idiom for get object or 404
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question}) # context is inside
+	def get_queryset(self):
+		'''Get last five published questions'''
+		return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    # response = "You're looking at the results of question %s"
-	# return HttpResponse(response % question_id)
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 def vote(request, question_id):
 	# return HttpResponse("You're voting on question %s" % question_id)
