861defe189434fbaf516a301f7fe4d4ce212393d
garrickf <gfaerr@stanford.edu>
2017-09-14 22:54:32 -0700

    edit admin panel

--- mysite_project/polls/admin.py
+++ mysite_project/polls/admin.py
@@ -3,7 +3,18 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
+
+# organize fields.
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	fields = ['pub_date', 'question_text']
+	inlines = [ChoiceInline]
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	list_filter = ['pub_date']
 
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
